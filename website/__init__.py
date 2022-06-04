0b058f575ac694ce9be6f85fc0d1900fd2ae90fe
some0ne14 <96661853+some0ne14@users.noreply.github.com>
2022-06-04 17:22:28 -0600

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
  
 def create_app(): 
     app = Flask(__name__) 
-    app.config['SECRET_KEY'] = 'SSn7RftwbH6pCYZeJwDujcylPwtQBrsG4AN7V4IQ'
+    app.config['SECRET_KEY'] = 'YOU_DO_NOT_KNIW'
 
     from .views import views
 
